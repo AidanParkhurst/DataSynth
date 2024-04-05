62b333cc96b3540a5d82b61af29bd68bde31a5ff
Aidan <aidanparkhurst@gmail.com>
2024-04-05 15:51:04 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Data Synth (Working Name)
+
+This is a **synthetic data generation tool** powered by AI! It's created using **Sveltekit**
+
+## Setup & Running
+To get started with the project, `git clone` this repository, then cd into the created folder and run `npm install`.
+
+After that's done, you can run at any time using `npm run dev`, and connecting via your browser to the link it outputs.
+
+If you want to test the web app on mobile, connect to the same wifi as your phone (or in the case of public wifi, connect to your phone's hotspot), and run `npm run dev -- --host`, which will provide an exposed address to connect from any device.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,193 @@
+<script>
+    import { writable } from 'svelte/store';
+
+    let mouse = writable({ x: 0, y: 0 });
+
+    function mouseMove(e) {
+        const rect = e.currentTarget.getBoundingClientRect()
+        mouse.set({ x: e.clientX - rect.x, y: e.clientY - rect.y });
+        e.currentTarget.style.setProperty('--x', `calc(${$mouse.x}px - 50vw)`);
+        e.currentTarget.style.setProperty('--y', `calc(${$mouse.y}px - 10vh)`);
+    }
+</script>
+
+<body>
+    <div on:mousemove={mouseMove} class="header" role="presentation">
+        <div class="logo">
+            <h2>Logo</h2>
+        </div>
+        <div class="links">
+            <a href="/">Product</a>
+            <a href="/">Demo</a>
+            <a href="/">Team</a>
+            <a href="/">Contact</a>
+        </div>
+        <div class="login">
+            <a href="/">Login</a>
+        </div>
+    </div>
+    <slot></slot>
+    <div class="footer">
+        <div class="logo">
+            <h2>Logo</h2>
+        </div>
+        <div class="rights">
+            <h3>DataSynth© 2024 All rights reserved</h3>
+        </div>
+        <div class="links">
+            <a href="/">Product</a>
+            <a href="/">Demo</a>
+            <a href="/">Team</a>
+            <a href="/">Contact</a>
+        </div>
+    </div>
+</body>
+
+<style>
+    :root {
+        --primary-color: #F9F9FF;
+        --secondary-color: #C9C9CF;
+        --border-color: #F9F9FF33;
+        --elevated-color: #0A0C1C;
+        --background-color: #000212;
+    }
+
+    * {
+        text-rendering: optimizeLegibility;
+        scroll-behavior: smooth;
+    }
+
+    body {
+        position:absolute;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        top: 0;
+        left: 0;
+        background-color: var(--background-color);
+        font-family: 'greycliff-cf', sans-serif;
+        color: var(--secondary-color);
+        overflow-x: hidden;
+    }
+
+    .header {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+        
+        width: 80vw;
+        height: calc(10vh + 1px);
+        padding: 0 10vw;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        overflow: hidden;
+
+        --x: 0;
+        --y: 0;
+    }
+    /* Soft glow which follows the mouse */
+    .header:before {
+        content: "";
+        position:absolute;
+        transition: opacity 0.5s; 
+
+        top:0;
+        left:0;
+        height: 200%;
+        width: 100%;
+        opacity: 0;
+        
+        background: radial-gradient(circle, var(--border-color) 0%, transparent 10%);
+        background-position-x: var(--x);
+        background-position-y: var(--y);
+        background-repeat: no-repeat;
+        
+        pointer-events: none;
+        touch-action: none;
+    }   
+    .header:hover:before {
+        opacity: 30%;
+    }
+    /* Gradient border at bottom */
+    .header:after {
+        position:absolute;
+        top: 10vh;
+        left: 0;
+        content: "";
+        width: 100%;
+        height: 1px;
+        background: radial-gradient(circle, var(--border-color) 0%, transparent 100%);
+    }
+    a {
+        transition: all 0.5s;
+    }
+    .links {
+        display: flex;
+        justify-content: space-between;
+        width: 50%;
+    }
+    .links a {
+        text-decoration: none;
+        color: var(--secondary-color);
+        padding: 5px 10px;
+    }
+    .links a:hover {
+        color: var(--primary-color);
+    }
+
+    .login a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        text-decoration: none;
+        color: var(--secondary-color);
+        padding: 10px 20px;
+        border-radius: 10px;
+        border: 1px solid var(--border-color);
+        /* slight glow */
+        box-shadow: 0 0 10px var(--border-color);
+    }
+    .login a:hover {
+        color: var(--background-color);
+        background: var(--secondary-color);
+    }
+    .login a:active {
+        background: var(--primary-color);
+    }
+
+    .footer {
+        position: relative;
+        width: 80vw;
+        height: 20vh;
+        z-index: 1;
+        
+        margin-top: 10vh;
+        padding: 5vh 10vw;
+        
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .footer .links {
+        display: flex;
+        flex-direction: column;
+        width: fit-content;
+    }
+    /* Outline border at top */
+    .footer:before {
+        content: "";
+        position: absolute;
+        top: -1px;
+        left: 0;
+        z-index: -2;
+
+        width: 100%;
+        height: 1px;
+        margin: 0;
+        background: radial-gradient(circle, var(--border-color) 0%, transparent 100%);
+    }
+</style>
